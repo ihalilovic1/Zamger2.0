416353df922e0bc900dcbd2b7f6a6454cc072cd4
Edina <edinaosmanspahic@gmail.com>
2021-01-07 23:46:33 +0100

    post metoda za kreiranje ispita

---
+++ Zamger2.0/Migrations/20210107222255_db-v4.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Zamger2._0.Migrations
+{
+    public partial class dbv4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Exams_AspNetUsers_StudentId",
+                table: "Exams");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Exams_StudentId",
+                table: "Exams");
+
+            migrationBuilder.DropColumn(
+                name: "StudentId",
+                table: "Exams");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StudentId",
+                table: "Exams",
+                type: "varchar(85)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exams_StudentId",
+                table: "Exams",
+                column: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Exams_AspNetUsers_StudentId",
+                table: "Exams",
+                column: "StudentId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- Zamger2.0/Models/Exam.cs
+++ Zamger2.0/Models/Exam.cs
@@ -13,18 +13,14 @@ namespace Zamger2._0.Models
         [BindProperty]
         [Required]
         [MinLength(4)]
-        public string Description { get; set; }
+        public string Name { get; set; }
         [BindProperty]
         [Required]
         public DateTime Deadline { get; set; }
         [Required]
         public string Subject { get; set; }
-        public List<SelectListItem> Subjects { get; set; } = new List<SelectListItem>
-        {
-            new SelectListItem { Value = "1", Text = "Predmet1" },
-            new SelectListItem { Value = "2", Text = "Predmet2" },
-            new SelectListItem { Value = "3", Text = "Predmet3"  },
-        };
+        public List<SelectListItem> Subjects { get; set; } = new List<SelectListItem>();
+        
     }
     
 }
